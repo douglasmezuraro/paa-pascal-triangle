eac9f8c59d051e5d00238206514b851e4603fc9a
Douglas Mezuraro <douglasmez@gmail.com>
2018-12-08 16:33:13 -0200

    Melhorado o visualizador de triângulo de Pascal

--- src/Forms.Main.pas
+++ src/Forms.Main.pas
@@ -8,6 +8,7 @@ uses
   System.SysUtils,
   System.Variants,
   System.Classes,
+  System.Math,
   Vcl.Graphics,
   Vcl.Controls,
   Vcl.Forms,
@@ -28,10 +29,14 @@ type
     procedure ActionCalculateExecute(Sender: TObject);
     procedure FormShow(Sender: TObject);
   private
+    FMatrix: TMatrix;
+    function FormatElement(const Element: Integer): string;
+    function LengthOfHighestElement: Integer;
     function GetInput: Integer;
     procedure SetInput(const Value: Integer);
   public
     property Input: Integer read GetInput write SetInput;
+    property Matrix: TMatrix read FMatrix write FMatrix;
   end;
 
 implementation
@@ -40,7 +45,6 @@ implementation
 
 procedure TMain.ActionCalculateExecute(Sender: TObject);
 var
-  Matrix: TMatrix;
   Line, Column: Integer;
   Element, Elements: string;
 begin
@@ -49,9 +53,9 @@ begin
   for Line := Low(Matrix) to High(Matrix) do
   begin
     Elements := string.Empty;
-    for Column := Low(Matrix) to High(Matrix) do
+    for Column := Low(Matrix) to Pred(Input) - Line do
     begin
-      Element := Format('[%d]', [Matrix[Line][Column]]);
+      Element := FormatElement(Matrix[Line][Column]);
       Elements := Elements + Element;
     end;
     Output.Lines.Insert(Line, Elements);
@@ -63,6 +67,25 @@ begin
   Input := 0;
   Output.Clear;
   InputEdit.SetFocus;
+end;
+
+function TMain.FormatElement(const Element: Integer): string;
+var
+  Length: Integer;
+  Mask: string;
+begin
+  Length := LengthOfHighestElement;
+  Mask := '%.' + Length.ToString + 'd ';
+  Result := Format(Mask, [Element]);
+end;
+
+function TMain.LengthOfHighestElement: Integer;
+var
+  Middle, Element: Integer;
+begin
+  Middle := Ceil(Input / 2);
+  Element := Matrix[Middle][Middle];
+  Result := Length(Element.ToString);
 end;
 
 function TMain.GetInput: Integer;
