e786b485fd81f2921fdc5d9c82fbed91b640f090
Douglas Mezuraro <douglasmez@gmail.com>
2018-12-13 13:57:41 -0200

    Inserido link do repositório do github

--- Pascal.Triangle.dpr
+++ Pascal.Triangle.dpr
@@ -1,5 +1,6 @@
 // Autor: Douglas Mezuraro - RA: 95676 - email: ra95676@uem.com.br
 // Autor: Ronnie Petterson Bueno - RA: 91938 - email: ra91938@uem.com.br
+// Repositório: https://github.com/douglasmezuraro/paa-pascal-triangle/
 
 program Pascal.Triangle;
 
